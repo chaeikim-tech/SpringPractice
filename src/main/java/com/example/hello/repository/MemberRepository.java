234f1678a7f8bb594be44b06f74eb33a27af7a7a
chaeikim <sb02348@hanmail.net>
2022-08-10 14:14:04 +0900

    Add Repository

---
+++ src/main/java/com/example/hello/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package com.example.hello.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.hello.domain.Member;
+
+public interface MemberRepository {
+	Member save(Member member);
+	Optional<Member> findById(long id);
+	Optional<Member> findByName(String name);
+	List<Member> findAll();
+}
